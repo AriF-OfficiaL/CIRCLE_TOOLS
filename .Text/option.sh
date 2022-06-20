dbe1876c59acabda8c099d775d5c1579be0db323
ARU <aru.ofc@yahoo.com>
2022-06-21 00:02:40 +0600

    ARU_CIRCLE_TOOLS

---
+++ .Text/option.sh
@@ -0,0 +1,16 @@
+
+     [1] API Key Generate
+     [2] ID to Number
+     [3] Number To ID
+     [4] Chek Following List
+     [5] Chek Follower List
+     [6] Clean Following
+     [7] Send Cpoke
+     [8] Send CCom
+     [9] Auto Shout
+     [10] Change Gender
+     [11] Change Birthday
+     [12] Change Nickname
+     [13] Show All Shout
+     
+     
